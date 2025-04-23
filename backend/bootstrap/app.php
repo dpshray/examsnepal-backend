9e92685216a649b08fc27562c53e7d025a697c65
DESKTOP-QCFO6Q2\NIKESH <nekesmgr@gmail.com>
2025-04-23 13:53:13 +0545

    changes for handling api exceptions

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -4,7 +4,12 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\AuthenticateApi; // Import the custom API authentication middleware
-use App\Http\Middleware\CheckUserRole; 
+use App\Http\Middleware\CheckUserRole;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -26,4 +31,27 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         // Handle global exceptions here (e.g., logging, custom responses)
+        // $exceptions->render(function (TransportException $e, Request $request) {
+        //     if ($request->expectsJson()) {
+        //         return sendJsonError('Connection could not be established');
+        //     }
+        // });
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            if ($request->expectsJson()) {
+                return Response::apiError('Not Found');
+            }
+        });
+        $exceptions->render(function (ValidationException $e, Request $request) {
+            if ($request->expectsJson()) {
+                $flattenedErrors = collect($e->errors())->map(function ($messages) {
+                    return $messages[0]; // get first error message per field
+                });
+                return Response::apiError('Validation Error', $flattenedErrors);
+            }
+        });
+        $exceptions->render(function (HttpException $e, Request $request) {
+            if ($request->expectsJson()) {
+                return Response::apiError($e->getMessage());
+            }
+    });
     })->create();
