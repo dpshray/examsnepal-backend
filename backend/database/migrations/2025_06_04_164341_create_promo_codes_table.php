0959c17ea445de8b2681a09ba75847f798808985
DESKTOP-QCFO6Q2\NIKESH <nekesmgr@gmail.com>
2025-08-18 17:29:56 +0545

    code updated

--- backend/database/migrations/2025_06_04_164341_create_promo_codes_table.php
+++ backend/database/migrations/2025_06_04_164341_create_promo_codes_table.php
@@ -3,6 +3,7 @@
 use App\Models\ExamType;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -20,6 +21,24 @@ return new class extends Migration
             $table->boolean('status')->default(1);
             $table->timestamps();
         });
+
+        if (Schema::hasTable('promo_codes')) {
+            DB::table('promo_codes')->insert([
+                [
+                    'code' => 'DWORK2025',
+                    'discount_percent' => 5,
+                    'detail' => 'DWORK FESTIVAL'
+                ],[
+                    'code' => 'KATHMANDUWEAR2081',
+                    'discount_percent' => 2,
+                    'detail' => 'KATHMANDU WEAR 2081'
+                ],[
+                    'code' => 'DASHAIN-2081',
+                    'discount_percent' => 3,
+                    'detail' => 'DASHAIN 2081'
+                ],
+            ]);
+        }
     }
 
     /**
