5d35329f79396b6e7f7f9a1d197535b54b6248b1
DESKTOP-QCFO6Q2\NIKESH <nekesmgr@gmail.com>
2025-09-04 16:44:22 +0545

    subscription message changed

--- backend/app/Http/Middleware/isStudentSubscribedMiddleware.php
+++ backend/app/Http/Middleware/isStudentSubscribedMiddleware.php
@@ -21,7 +21,7 @@ class isStudentSubscribedMiddleware
             return response()->json([
                 'status' => false,
                 'data' => null,
-                'message' => 'user has not been subscribed'
+                'message' => 'You do not have an active subscription.'
             ], 403);
         }
         return $next($request);
