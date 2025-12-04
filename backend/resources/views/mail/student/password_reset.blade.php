afa2241a19507c8770b66d3f2bacae0d446d33b7
Soyamstha <shrestha.soyam111@gmail.com>
2025-12-04 09:34:18 +0545

    remove backend folder

--- backend/resources/views/mail/student/password_reset.blade.php
+++
@@ -1,2 +0,0 @@
-Hello,  {{ $user }},
-Your token for password reset is {{ $token }}(this token expires at {{ $expiration_period }})
--- backend/resources/views/mail/student/register.blade.php
+++
@@ -1,3 +0,0 @@
-Welcome {{ ucwords($name) }}! 
-you can verify this email by visiting this <a href="{{ html_entity_decode($url) }}" title="">link</a>
-(this link expires at {{ $expiration_period }})
