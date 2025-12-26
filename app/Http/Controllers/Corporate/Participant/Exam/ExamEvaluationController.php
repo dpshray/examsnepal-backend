7ecb45d7386e9c21eccda12f719d59f4d668a8bf
Soyamstha <shrestha.soyam111@gmail.com>
2025-12-26 15:46:37 +0545

    pull from main

---
+++ app/Http/Controllers/Corporate/Participant/Exam/ExamEvaluationController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Corporate\Participant\Exam;
+
+use App\Http\Controllers\Controller;
+use App\Models\ExamAttempt;
+use Illuminate\Http\Request;
+
+class ExamEvaluationController extends Controller
+{
+    //
+    function evaluating(ExamAttempt $attempt)
+    {
+
+    }
+}
