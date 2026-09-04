876120cf0d4664e6b9643ac68583fcf4e69cb4da
Dipesh Ray <dpshray@gmail.com>
2026-09-05 00:47:11 +0545

    fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
         $this->call(AdminSeeder::class);
+        $this->call(ExamGuideSeeder::class);
         // User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
